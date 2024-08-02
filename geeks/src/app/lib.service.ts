bf5b76dfcf6e289dc22e97c0b8f36e5a0c91530a
Jeevitha771 <81794463+Jeevitha771@users.noreply.github.com>
2024-08-02 16:10:37 +0530

    Update lib.service.ts

--- geeks/src/app/lib.service.ts
+++ geeks/src/app/lib.service.ts
@@ -8,7 +8,7 @@ import { LibModel } from './lib.model';
 export class LibService {
   constructor(private http: HttpClient) {}
 
-  baseurl: string = 'http://localhost:3000/';
+  baseurl: string = 'https://mean3.onrender.com/';
 
   getAllLibs() {
     return this.http.get<LibModel[]>(this.baseurl + 'Allbooks');
